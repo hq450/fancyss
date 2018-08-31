eaffb53109ed8085239214ffcce9e7912448b5fc
hq450 <qhuang450@gmail.com>
2018-09-01 00:43:28 +0800

    修复一个因获取到错误的base64字符串长度而导致订阅节点信息错误，最终导致订阅完成后部分节点被删除的bug

--- fancyss_arm/shadowsocks/scripts/ss_online_update.sh
+++ fancyss_arm/shadowsocks/scripts/ss_online_update.sh
@@ -151,13 +151,12 @@ prepare(){
 
 
 decode_url_link(){
-	link=$1
-	num=$2
-	len=$((${#link}-$num))
-	mod4=$(($len%4))
+	local link=$1
+	local len=`echo $link| wc -L`
+	local mod4=$(($len%4))
 	if [ "$mod4" -gt "0" ]; then
-		var="===="
-		newlink=${link}${var:$mod4}
+		local var="===="
+		local newlink=${link}${var:$mod4}
 		echo -n "$newlink" | sed 's/-/+/g; s/_/\//g' | base64 -d 2>/dev/null
 	else
 		echo -n "$link" | sed 's/-/+/g; s/_/\//g' | base64 -d 2>/dev/null
@@ -202,33 +201,32 @@ get_remote_config(){
 	protocol=$(echo "$decode_link" |awk -F':' '{print $3}')
 	encrypt_method=$(echo "$decode_link" |awk -F':' '{print $4}')
 	obfs=$(echo "$decode_link" |awk -F':' '{print $5}'|sed 's/_compatible//g')
-	#password=$(echo "$decode_link" |awk -F':' '{print $6}'|awk -F'/' '{print $1}')
 	
-	password=$(decode_url_link $(echo "$decode_link" |awk -F':' '{print $6}'|awk -F'/' '{print $1}') 0)
+	password=$(decode_url_link $(echo "$decode_link" |awk -F':' '{print $6}'|awk -F'/' '{print $1}'))
 	password=`echo $password|base64_encode`
 	
 	obfsparam_temp=$(echo "$decode_link" |awk -F':' '{print $6}'|grep -Eo "obfsparam.+"|sed 's/obfsparam=//g'|awk -F'&' '{print $1}')
-	[ -n "$obfsparam_temp" ] && obfsparam=$(decode_url_link $obfsparam_temp 0) || obfsparam=''
+	[ -n "$obfsparam_temp" ] && obfsparam=$(decode_url_link $obfsparam_temp) || obfsparam=''
 	
 	protoparam_temp=$(echo "$decode_link" |awk -F':' '{print $6}'|grep -Eo "protoparam.+"|sed 's/protoparam=//g'|awk -F'&' '{print $1}')
-	[ -n "$protoparam_temp" ] && protoparam=$(decode_url_link $protoparam_temp 0|sed 's/_compatible//g') || protoparam=''
+	[ -n "$protoparam_temp" ] && protoparam=$(decode_url_link $protoparam_temp|sed 's/_compatible//g') || protoparam=''
 	
 	remarks_temp=$(echo "$decode_link" |awk -F':' '{print $6}'|grep -Eo "remarks.+"|sed 's/remarks=//g'|awk -F'&' '{print $1}')
 	if [ "$action" == "1" ];then
 		#订阅
-		[ -n "$remarks_temp" ] && remarks=$(decode_url_link $remarks_temp 0) || remarks=""
+		[ -n "$remarks_temp" ] && remarks=$(decode_url_link $remarks_temp) || remarks=""
 	elif [ "$action" == "2" ];then
 		# ssr://添加
-		[ -n "$remarks_temp" ] && remarks=$(decode_url_link $remarks_temp 0) || remarks='AutoSuB'
+		[ -n "$remarks_temp" ] && remarks=$(decode_url_link $remarks_temp) || remarks='AutoSuB'
 	fi
 	
 	group_temp=$(echo "$decode_link" |awk -F':' '{print $6}'|grep -Eo "group.+"|sed 's/group=//g'|awk -F'&' '{print $1}')
 	if [ "$action" == "1" ];then
 		#订阅
-		[ -n "$group_temp" ] && group=$(decode_url_link $group_temp 0) || group=""
+		[ -n "$group_temp" ] && group=$(decode_url_link $group_temp) || group=""
 	elif [ "$action" == "2" ];then
 		# ssr://添加
-		[ -n "$group_temp" ] && group=$(decode_url_link $group_temp 0) || group='AutoSuBGroup'
+		[ -n "$group_temp" ] && group=$(decode_url_link $group_temp) || group='AutoSuBGroup'
 	fi
 
 	[ -n "$group" ] && group_base64=`echo $group | base64_encode | sed 's/ -//g'`
@@ -452,7 +450,7 @@ get_oneline_rule_now(){
 		echo_date 下载订阅成功...
 		echo_date 开始解析节点信息...
 		#cat /tmp/ssr_subscribe_file.txt | base64 -d > /tmp/ssr_subscribe_file_temp1.txt
-		decode_url_link `cat /tmp/ssr_subscribe_file.txt` 0 > /tmp/ssr_subscribe_file_temp1.txt
+		decode_url_link `cat /tmp/ssr_subscribe_file.txt` > /tmp/ssr_subscribe_file_temp1.txt
 		# 检测ss ssr
 		NODE_FORMAT1=`cat /tmp/ssr_subscribe_file_temp1.txt | grep -E "^ss://"`
 		NODE_FORMAT2=`cat /tmp/ssr_subscribe_file_temp1.txt | grep -E "^ssr://"`
@@ -463,16 +461,16 @@ get_oneline_rule_now(){
 			NODE_NU=`cat /tmp/ssr_subscribe_file_temp1.txt | grep -c "ssr://"`
 			echo_date 检测到ssr节点格式，共计$NODE_NU个节点...
 			#判断格式
-			maxnum=$(decode_url_link `cat /tmp/ssr_subscribe_file.txt` 0 | grep "MAX=" | awk -F"=" '{print $2}' | grep -Eo "[0-9]+")
+			maxnum=$(decode_url_link `cat /tmp/ssr_subscribe_file.txt` | grep "MAX=" | awk -F"=" '{print $2}' | grep -Eo "[0-9]+")
 			if [ -n "$maxnum" ]; then
-				urllinks=$(decode_url_link `cat /tmp/ssr_subscribe_file.txt` 0 | sed '/MAX=/d' | shuf -n $maxnum | sed 's/ssr:\/\///g')
+				urllinks=$(decode_url_link `cat /tmp/ssr_subscribe_file.txt` | sed '/MAX=/d' | shuf -n $maxnum | sed 's/ssr:\/\///g')
 			else
-				urllinks=$(decode_url_link `cat /tmp/ssr_subscribe_file.txt` 0 | sed 's/ssr:\/\///g')
+				urllinks=$(decode_url_link `cat /tmp/ssr_subscribe_file.txt` | sed 's/ssr:\/\///g')
 			fi
 			[ -z "$urllinks" ] && continue
 			for link in $urllinks
 			do
-				decode_link=$(decode_url_link $link 0)
+				decode_link=$(decode_url_link $link)
 				get_remote_config $decode_link 1
 				[ "$?" == "0" ] && update_config || echo_date "检测到一个错误节点，已经跳过！"
 			done
@@ -680,7 +678,7 @@ add() {
 			if [ -n "`echo -n "$ssrlink" | grep "ssr://"`" ]; then
 				echo_date 检测到SSR链接...开始尝试解析...
 				new_ssrlink=`echo -n "$ssrlink" | sed 's/ssr:\/\///g'`
-				decode_ssrlink=$(decode_url_link $new_ssrlink 1)
+				decode_ssrlink=$(decode_url_link $new_ssrlink)
 				get_remote_config $decode_ssrlink 2
 				add_ssr_servers 1
 			else
@@ -692,7 +690,7 @@ add() {
 					new_sslink=`echo -n "$ssrlink" | sed 's/ss:\/\///g'`
 					remarks='AddByLink'
 				fi
-				decode_sslink=$(decode_url_link $new_sslink 1)
+				decode_sslink=$(decode_url_link $new_sslink)
 				get_ss_config $decode_sslink
 				add_ss_servers
 			fi
