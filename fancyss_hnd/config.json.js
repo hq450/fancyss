294d25e9e1ca224895c4980c369250ba4c754bfa
hq450 <qhuang450@gmail.com>
2018-09-03 18:14:09 +0800

    hnd 1.3.9

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.3.8",
-"md5":"0eabb93067463de62c029bfe8df128e0",
+"version":"1.3.9",
+"md5":"1cde1a21b88a3523d918a81638e14fc5",
 "home_url":"Module_shadowsocks.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-09-02_00:29:56"
+"build_date":"2018-09-03_18:14:01"
 }
