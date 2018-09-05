dc04c0b7d95e8975569415589ff1580d108de057
hq450 <qhuang450@gmail.com>
2018-09-05 21:00:39 +0800

    arm 4.0.9

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"4.0.8",
-"md5":"a05e136eaaeaf2b0cfede178afd6135f",
+"version":"4.0.9",
+"md5":"a5b9ddf48424f72ab6f559264a423077",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-09-04_17:43:16"
+"build_date":"2018-09-05_21:00:25"
 }
