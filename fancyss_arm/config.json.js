5ed4f477303180364fbf72f3a07e935407650e33
hq450 <qhuang450@gmail.com>
2018-09-11 01:43:01 +0800

    arm 4.1.1; hnd 1.4.2

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"4.1.0",
-"md5":"d6f24368c5ca387d3b0b6cb0da8101b6",
+"version":"4.1.1",
+"md5":"caf5c05a22e7a4bb892e0bf650957b11",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-09-10_17:32:39"
+"build_date":"2018-09-11_01:42:09"
 }
--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"1.4.1",
-"md5":"5cd62c4cdfb6e922f6d891c115feff72",
+"version":"1.4.2",
+"md5":"d4ccda9a9afea21623f3af00c2b5d094",
 "home_url":"Module_shadowsocks.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-09-10_19:43:03"
+"build_date":"2018-09-11_01:42:30"
 }
