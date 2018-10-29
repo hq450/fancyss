f89dc68440ca43fdfa59087efe548253fac99bdb
hq450 <qhuang450@gmail.com>
2018-10-29 09:56:55 +0800

    refine

--- fancyss_arm/shadowsocks/scripts/ss_online_update.sh
+++ fancyss_arm/shadowsocks/scripts/ss_online_update.sh
@@ -303,7 +303,7 @@ get_v2ray_remote_config(){
 	decode_link="$1"
 	v2ray_group="$2"
 	v2ray_v=$(echo "$decode_link" | jq -r .v)
-	v2ray_ps=$(echo "$decode_link" | jq -r .ps)
+	v2ray_ps=$(echo "$decode_link" | jq -r .ps | sed 's/[ \t]*//g')
 	v2ray_add=$(echo "$decode_link" | jq -r .add | sed 's/[ \t]*//g')
 	v2ray_port=$(echo "$decode_link" | jq -r .port | sed 's/[ \t]*//g')
 	v2ray_id=$(echo "$decode_link" | jq -r .id | sed 's/[ \t]*//g')
@@ -312,6 +312,7 @@ get_v2ray_remote_config(){
 	v2ray_type=$(echo "$decode_link" | jq -r .type)
 	v2ray_tls_tmp=$(echo "$decode_link" | jq -r .tls)
 	[ "$v2ray_tls_tmp"x == "tls"x ] && v2ray_tls="tls" || v2ray_tls="none"
+	
 	if [ "$v2ray_v" == "2" ];then
 		#echo_date "new format"
 		v2ray_path=$(echo "$decode_link" |jq -r .path)
