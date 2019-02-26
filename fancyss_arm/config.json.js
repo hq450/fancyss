25f86d4b68748f5b99fc3e9989140c7e6e9e4efd
Acris Liu <acrisliu@gmail.com>
2019-02-26 12:15:47 +0000

    update shadowsocks.tar.gz

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2019-02-26_12:09:24",
+"build_date":"2019-02-26_20:11:23",
 "description":"科学上网",
 "home_url":"Main_Ss_Content.asp",
-"md5":"4638b7b8a445804a167d3aee0b0d9c4f",
+"md5":"0aa27270b5995525251c674ac963e278",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm/shadowsocks.tar.gz", 
 "title":"科学上网",
