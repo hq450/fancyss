03bf7f7012e2c7019079814d71703c048b8e108b
Acris Liu <acrisliu@gmail.com>
2019-02-26 09:34:54 +0000

    update shadowsocks-libev to v3.2.4

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2019-02-19_14:17:56",
+"build_date":"2019-02-26_09:33:20",
 "description":"科学上网",
 "home_url":"Main_Ss_Content.asp",
-"md5":"c66daddf779f47664517f9cc60a22339",
+"md5":"99ab54ab01aa86e0b1fc8f494e351d3d",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"4.1.9"
+"version":"4.1.8"
 }
