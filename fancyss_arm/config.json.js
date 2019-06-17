06e2ed610fe97ec35e815d3a904d7695bf05f975
hq450 <qhuang450@gmail.com>
2019-06-17 12:33:26 +0800

    fix

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2019-06-17_11:05:23",
+"build_date":"2019-06-17_12:31:21",
 "description":"科学上网",
 "home_url":"Main_Ss_Content.asp",
-"md5":"c2588993b4ac92c8dead460ff59df507",
+"md5":"91aa6074bd1fde01249af9bd7812479d",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"4.1.8"
+"version":"4.1.9"
 }
