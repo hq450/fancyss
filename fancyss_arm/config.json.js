f6a2faf6b1035d5a90c1f4a8ecb6a630cd67ad97
hq450 <qhuang450@gmail.com>
2019-06-17 11:05:52 +0800

    repack 4.1.8

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2019-06-17_10:47:52",
+"build_date":"2019-06-17_11:05:23",
 "description":"科学上网",
 "home_url":"Main_Ss_Content.asp",
-"md5":"54fd75f23a119c4d2fd56625252d9b7d",
+"md5":"c2588993b4ac92c8dead460ff59df507",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"4.1.7"
+"version":"4.1.8"
 }
