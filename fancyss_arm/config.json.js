60acda3635d9d77063a00620f2158a3882decdaa
Acris Liu <acrisliu@gmail.com>
2019-06-19 16:08:49 +0000

    pack

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2019-06-17_12:31:21",
+"build_date":"2019-06-19_16:08:19",
 "description":"科学上网",
 "home_url":"Main_Ss_Content.asp",
-"md5":"91aa6074bd1fde01249af9bd7812479d",
+"md5":"cabe82131501e91385fbe47f8fbfef44",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm/shadowsocks.tar.gz", 
 "title":"科学上网",
