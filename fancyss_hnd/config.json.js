2383388a6918b2ae98e72487ea5425328db4a725
hq450 <qhuang450@gmail.com>
2019-07-05 18:55:37 +0800

    fix hnd css loss

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2019-07-05_11:47:54",
+"build_date":"2019-07-05_18:54:42",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"789066cff2bbd6574e0c9eb74eb316b4",
+"md5":"28e3e7a2bc2b3c138a80a6a59dadd126",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"1.6.9"
+"version":"1.7.0"
 }
