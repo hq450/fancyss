0cc29cc7212f09cf8c02983c857d1b1d4f0dadee
hq450 <qhuang450@gmail.com>
2019-08-22 13:56:37 +0800

    update fancyss_arm384 to 1.0.3

--- fancyss_arm384/config.json.js
+++ fancyss_arm384/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2019-07-05_11:51:39",
+"build_date":"2019-08-22_13:56:07",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"aff2ee16426b7f0ce19805ba51fa8cf9",
+"md5":"bdedf55e87d341b503642f5f590b0ff0",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm384/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"1.0.2"
+"version":"1.0.3"
 }
