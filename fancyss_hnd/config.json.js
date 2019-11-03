f2fc6691bfabcf2f2dd52834143184678e0c2471
Zed <zed@zedd.dev>
2019-11-03 19:45:45 +0800

    bump ss-local/redir/tunnel version to 3.3.3;

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2019-10-31_22:29:20",
+"build_date":"2019-11-03_19:42:57",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"51132d7d7a374e64726540c9152220a5",
+"md5":"1fdd34b29ad70e539ba20391b8ea2a1c",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
