4dc74f0d24521acb83ac643cc7ffffe628daad11
Zed <zed@zedd.dev>
2019-11-10 16:49:15 +0800

    hnd: bump v2ray version to 4.21.3

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2019-11-03_19:42:57",
+"build_date":"2019-11-10_16:48:19",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"1fdd34b29ad70e539ba20391b8ea2a1c",
+"md5":"d5185f80e571ec95ae4de2c1fac4ab7f",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
