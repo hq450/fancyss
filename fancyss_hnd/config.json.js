17e3ba93530f2a022dc64f839661058a2a2bdbe9
Zed <zed@zedd.dev>
2019-11-23 11:50:05 +0800

    hnd: 更新正确的版本号显示

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2019-11-23_11:32:50",
+"build_date":"2019-11-23_11:48:20",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"4a1aac43de445192e3beb0d246893526",
+"md5":"2b20cd583f69e2f584424315924e5799",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
