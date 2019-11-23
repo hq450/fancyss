b38d57202968d862df3cf32dfde037fa1b94c920
Zed <zed@zedd.dev>
2019-11-23 13:13:23 +0800

    hnd: 1.7.6

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2019-11-23_12:01:45",
+"build_date":"2019-11-23_13:12:44",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"1699c03f9a36c4e78f42d4eb6e6d6289",
+"md5":"8607fab7e882de23a345e272e3358e34",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"1.7.5"
+"version":"1.7.6"
 }
