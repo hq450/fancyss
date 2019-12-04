d3aa01029aaee55531953048f046d967f8c72679
hq450 <qhuang450@gmail.com>
2019-12-04 23:34:28 +0800

    fancyss_hnd 1.7.9 add SmartDNS#2

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2019-12-04_23:32:01",
+"build_date":"2019-12-04_23:34:19",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"ce434492c8f52c8cc72916d314d03526",
+"md5":"de5346063eac92b756c45faae4f1d543",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
