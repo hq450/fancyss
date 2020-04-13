c17c01f9e39528090308cf90d6c6c3b276a2ab81
babylife <31977161+babylife@users.noreply.github.com>
2020-04-13 09:45:04 +0800

    update the history download when check version
    change to https://github.com/idealism-xxm/fancyss/tree/master/fancyss_arm/history_package

--- fancyss_arm/shadowsocks/scripts/ss_update.sh
+++ fancyss_arm/shadowsocks/scripts/ss_update.sh
@@ -60,7 +60,7 @@ update_ss(){
 
 update_ss2(){
 	echo_date "目前还没有任何备用服务器！请尝试使用离线安装功能！"
-	echo_date "历史版本下载地址：https://github.com/hq450/fancyss/tree/master/fancyss_arm/history"
+	echo_date "历史版本下载地址：https://github.com/idealism-xxm/fancyss/tree/master/fancyss_arm/history_package"
 	echo_date "下载后请将下载包名字改为：shadowsocks.tar.gz，再使用离线安装进行安装"
 	sleep 1
 	exit
