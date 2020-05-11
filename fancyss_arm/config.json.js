7bc909d3a2ab4c887e3e95e28069771265407d23
idealism-xxm <biaochekuangmo@yeah.net>
2020-05-12 00:22:54 +0800

    fix(ss): fix subscribing trojan error
    fix #25, fix #27

--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2020-05-02_12:35:52",
+"build_date":"2020-05-12_00:18:52",
 "description":"科学上网",
 "home_url":"Main_Ss_Content.asp",
-"md5":"6b7baa69bbb5649ba764b8bc3168746d",
+"md5":"9dbe1df0dcd0f2883906dbc42aabb91f",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/idealism-xxm/fancyss/master/fancyss_arm/shadowsocks.tar.gz",
 "title":"科学上网",
-"version":"4.3.5"
+"version":"4.3.6"
 }
