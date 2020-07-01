e37c0fa7754a226cdca560e38f8df410f0c1c973
hq450 <qhuang450@gmail.com>
2020-07-01 15:03:40 +0800

    修复了插件不更新的bug

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2020-06-03_23:57:14",
+"build_date":"2020-07-01_15:00:42",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"eb181521c4b48a789a3d395fde10efb4",
+"md5":"9f21495b8b8d3692b8a49f3e9425155b",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"1.8.5"
+"version":"1.8.6"
 }
