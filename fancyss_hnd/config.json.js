086f112dc0c2a0eb3299a2ae9c8d0a19ad444364
hq450 <qhuang450@gmail.com>
2020-09-16 10:35:10 +0800

    bump version 1.8.7 for ZenWiFi AX（灵耀 AX6600M/RT-AX95Q）support

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2020-07-01_15:00:42",
+"build_date":"2020-09-16_10:34:19",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"9f21495b8b8d3692b8a49f3e9425155b",
+"md5":"6f2a309435b8cfcea1b5584add8488dd",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"1.8.6"
+"version":"1.8.7"
 }
