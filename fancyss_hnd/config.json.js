e72fb6b27bff99ba0124405a3296b883f336db7d
hq450 <qhuang450@gmail.com>
2020-09-17 10:49:06 +0800

    add ZenWiFi_XD4（灵耀AX魔方/RT-AX56_XD4）support

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2020-09-16_10:34:19",
+"build_date":"2020-09-17_10:48:06",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"6f2a309435b8cfcea1b5584add8488dd",
+"md5":"399a360da73637183d5e84ed45837016",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
