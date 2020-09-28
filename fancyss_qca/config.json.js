9876bd0afd69721e0a841325ccd713f9774e475a
hq450 <qhuang450@gmail.com>
2020-09-28 20:02:10 +0800

    update qca

--- fancyss_qca/config.json.js
+++ fancyss_qca/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2020-09-28_19:56:11",
+"build_date":"2020-09-28_20:01:52",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"505585e0932ca4638c5455180035c63b",
+"md5":"3975b03456255374642a57ed762cf2d1",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_qca/shadowsocks.tar.gz", 
 "title":"科学上网",
