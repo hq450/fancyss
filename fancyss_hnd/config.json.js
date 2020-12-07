be2d24ab70b4f992fbbffbc7f1f08d016b9fbca1
hq450 <qhuang450@gmail.com>
2020-12-07 23:22:12 +0800

    update fancyss_hnd 1.9.1

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2020-12-07_14:31:08",
+"build_date":"2020-12-07_23:21:56",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"b145000743f2b16381ae7d5e57c1be0d",
+"md5":"8c8a2f19975704df28e88c520f8b5d44",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"1.9.0"
+"version":"1.9.1"
 }
