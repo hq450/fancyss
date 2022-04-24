022fa207b1d0ec0caeeac52f3bcf07fd569482bc
hq450 <qhuang450@gmail.com>
2022-04-24 23:13:37 +0800

    use ipv4 for update

--- fancyss_hnd/shadowsocks/scripts/ss_update.sh
+++ fancyss_hnd/shadowsocks/scripts/ss_update.sh
@@ -33,7 +33,7 @@ update_ss(){
 			cd /tmp
 			md5_web1=`curl -4sk --connect-timeout 5 $main_url/version | sed -n 2p`
 			echo_date 开启下载进程，从主服务器上下载更新包...
-			wget --no-check-certificate --timeout=5 "$main_url"/shadowsocks.tar.gz
+			wget -4 --no-check-certificate --timeout=5 "$main_url"/shadowsocks.tar.gz
 			md5sum_gz=`md5sum /tmp/shadowsocks.tar.gz | sed 's/ /\n/g'| sed -n 1p`
 			if [ "$md5sum_gz" != "$md5_web1" ]; then
 				echo_date 更新包md5校验不一致！估计是下载的时候出了什么状况，请等待一会儿再试...
