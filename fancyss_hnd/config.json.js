57e01330022abb35a69e807f834d99ef6d7f21ed
hq450 <qhuang450@gmail.com>
2022-04-26 00:29:44 +0800

    repack

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2022-04-26_00:21:11",
+"build_date":"2022-04-26_00:29:05",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"ee46d8691a293047785ef91dbf7a2ab5",
+"md5":"9c99793f3b72a964805f25bc798cb870",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
--- fancyss_qca/config.json.js
+++ fancyss_qca/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2022-04-26_00:21:27",
+"build_date":"2022-04-26_00:29:11",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"3de55d1299c1d4a318f073916210dd3a",
+"md5":"4a7fd6a882f88e670288dd35a34d92cd",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_qca/shadowsocks.tar.gz", 
 "title":"科学上网",
