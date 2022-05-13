300d96d523c86238d9c6828a648d41c7102ca747
hq450 <qhuang450@gmail.com>
2022-05-13 23:55:07 +0800

    adapt more ss airport & use server ip under trojan

--- fancyss_qca/shadowsocks/scripts/ss_online_update.sh
+++ fancyss_qca/shadowsocks/scripts/ss_online_update.sh
@@ -393,7 +393,7 @@ get_ss_node(){
 	if [ -n "${plugin_support}" ];then
 		obfs_para=$(echo "${urllink}" | sed -n 's/.\+plugin=\(\)/\1/p'|sed 's/@/|/g;s/:/|/g;s/?/|/g;s/#/|/g' | awk -F'|' '{print $1}'| urldecode)
 		plugin_prog=$(echo "${obfs_para}" | awk -F';' '{print $1}')
-		if [ "${plugin_prog}" == "obfs-local" ];then
+		if [ "${plugin_prog}" == "obfs-local" -o "${plugin_prog}" == "simple-obfs" ];then
 			ss_obfs=$(echo "${obfs_para}" | awk -F';' '{print $2}'| awk -F'=' '{print $2}')
 			ss_obfs_host=$(echo "${obfs_para}" | awk -F';' '{print $3}'| awk -F'=' '{print $2}')
 			ss_v2ray=""
