78c5f83ca98fdabe49f1aba37c6f17e928c481a6
hq450 <qhuang450@gmail.com>
2022-05-26 23:09:41 +0800

    test for version file

---
+++ packages/version.json.js
@@ -0,0 +1,10 @@
+{
+  "name": "fancyss",
+  "version": "2.0.2",
+  "md5_hnd_full": "91547731424dce71953fe538dac51175",
+  "md5_hnd_lite": "4379830aa76a850c8bfa8cd71f43ff6a",
+  "md5_qca_full": "f011d392e0a7b5c76f0467c4460f4d31",
+  "md5_qca_lite": "b4a126d49d17223b5993bfc9be453b07",
+  "md5_arm_full": "d9e23a62fe3f7b9d604000a57ae244f0",
+  "md5_arm_lite": "94423aa1af3d077c6a71110f8132565a"
+}
