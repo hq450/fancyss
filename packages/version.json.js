97e9057196ff88e3c9f8f0b9fe3e6518fdda89ac
hq450 <qhuang450@gmail.com>
2024-02-29 22:51:05 +0800

    update 3.3.0

--- packages/version.json.js
+++ packages/version.json.js
@@ -1,14 +1,14 @@
 {
   "name": "fancyss",
-  "version": "3.2.9",
-  "md5_hnd_full": "0c87b9ac3b08684be9af432434028bb6",
-  "md5_hnd_lite": "b60c41ebe4e007006915140c98987d06",
-  "md5_hnd_v8_full": "876baf571886fe72e16a92b27c5f3e9b",
-  "md5_hnd_v8_lite": "4f4dfe7d19f361ba571705abeb2aed75",
-  "md5_qca_full": "233c2c28eb2872c584fa7161ff24cfd7",
-  "md5_qca_lite": "f249ede65f3200d6dfe94321a8832e73",
-  "md5_arm_full": "59088eba35652c8d44796d59cef56e1b",
-  "md5_arm_lite": "7f37c25dd65c6ec1edfa2f8ba70abdbf",
-  "md5_mtk_full": "6b3141c33eb3541e4b17f29e51c70792",
-  "md5_mtk_lite": "9e97a936f840efb5f6d1e81f4248f766"
+  "version": "3.3.0",
+  "md5_hnd_full": "bf168b1b746105afe56ff6c7bc2637d7",
+  "md5_hnd_lite": "62d9bcdddfdf97733764498128668f12",
+  "md5_hnd_v8_full": "da5bc0900d0688bec0a3dd5996632568",
+  "md5_hnd_v8_lite": "d1ed8689b15833f1f6ef8a1d439f9259",
+  "md5_qca_full": "17b71cd7e72335c3e13623d1f00c866f",
+  "md5_qca_lite": "1daff311987217896e29c2a3364d6e6e",
+  "md5_arm_full": "4ebc1d13bf4330abb1486f4763e693d1",
+  "md5_arm_lite": "fff730f57b30747059745de0f8552503",
+  "md5_mtk_full": "0be274dd7417515c5a9cf8a04ee1e5bd",
+  "md5_mtk_lite": "478e506606d00827146f0348690346d2"
 }
