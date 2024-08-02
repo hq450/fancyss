f5dc75fa3a56707aeffc701ee9c618399ea74af3
hq450 <qhuang450@gmail.com>
2024-08-02 10:09:14 +0800

    update rules

--- rules/rules.json.js
+++ rules/rules.json.js
@@ -1,59 +1,59 @@
 {
   "gfwlist": {
     "name": "gfwlist.conf",
-    "date": "2024-03-02 22:27",
-    "md5": "65462d3b1acbf6d6c776098d52b00735",
-    "count": "4872"
+    "date": "2024-08-02 10:08",
+    "md5": "9715108cfc71d816e67ac9381b2b700f",
+    "count": "4950"
   },
   "chnroute": {
     "name": "chnroute.txt",
-    "date": "2024-03-02 22:27",
-    "md5": "5a9a467fef8f5f073f49f27c8fe97a16",
-    "count": "3502",
-    "count_ip": "297431204",
+    "date": "2024-08-02 10:08",
+    "md5": "3749b437d60201bb58668d93fe716961",
+    "count": "3607",
+    "count_ip": "283723730",
     "source": "misakaio",
     "url": "https://github.com/misakaio/chnroutes2/blob/master/chnroutes.txt"
   },
   "chnroute2": {
     "name": "chnroute2.txt",
-    "date": "2024-02-05 21:33",
-    "md5": "8f666fc3ea51785c775b606a7fba27a2",
-    "count": "6817",
-    "count_ip": "374016518582",
+    "date": "2024-08-02 10:08",
+    "md5": "655a698f031681c2547647d0b2e2aad0",
+    "count": "6884",
+    "count_ip": "386901474894",
     "source": "ipip",
     "url": "https://github.com/firehol/blocklist-ipsets/blob/master/ipip_country/ipip_country_cn.netset"
   },
   "chnroute3": {
     "name": "chnroute3.txt",
-    "date": "2024-03-02 22:27",
-    "md5": "db6ca8e5b7e1fef182b38a939df49414",
-    "count": "8664",
-    "count_ip": "343068752",
+    "date": "2024-08-02 10:08",
+    "md5": "f4f25f25d6f8cf16c21deefdaaf0af27",
+    "count": "8658",
+    "count_ip": "343129692",
     "source": "apnic",
     "url": "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
   },
   "cdn_china": {
     "name": "cdn.txt",
-    "date": "2024-03-02 22:27",
-    "md5": "268f471b025da00d381d27419d622b45",
-    "count": "72021"
+    "date": "2024-08-02 10:08",
+    "md5": "6e4cb0a95174e72c405c59d9d5f0550b",
+    "count": "87651"
   },
   "apple_china": {
     "name": "apple_china.txt",
-    "date": "2024-02-05 21:33",
-    "md5": "c7b088a0e68d912cad8810f221098316",
-    "count": "141"
+    "date": "2024-08-02 10:08",
+    "md5": "63a97610980d5ca073a8ba49770f06e6",
+    "count": "171"
   },
   "google_china": {
     "name": "google_china.txt",
-    "date": "2024-03-02 22:27",
-    "md5": "f904f6e981f456ede3bf7bedcd017954",
-    "count": "190"
+    "date": "2024-08-02 10:08",
+    "md5": "6899bb37a930b7b4c89528035c90f5f4",
+    "count": "191"
   },
   "cdn_test": {
     "name": "cdn_test.txt",
-    "date": "2023-11-30 20:31",
-    "md5": "903ac79bac193dc59cb1eed69c7d6cc6",
-    "count": "82"
+    "date": "2024-08-02 10:08",
+    "md5": "110d8f1e1a3d6ff2d39fc568a2a5e792",
+    "count": "86"
   }
 }
